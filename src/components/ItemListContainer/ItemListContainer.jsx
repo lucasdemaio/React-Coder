bba27eadac6bd64ffa883bf6a56c6e2650ef90fe
Lucas De Maio <ldemaio@emaservicios.local>
2024-11-14 10:58:01 -0300

    Entrega Final

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,7 +3,7 @@ import ItemList from "./ItemList.jsx"
 import { collection, getDocs, query, where } from "firebase/firestore"
 import db from "../../db/db.js"
 import {useParams} from "react-router-dom"
-import "./itemlistcontainer.css"
+import './itemListaContainer.css'
 import Loading from "../Loading/Loading.jsx"
 
 
